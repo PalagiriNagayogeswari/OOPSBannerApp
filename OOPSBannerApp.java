610f27c3a1ed2483b0d62e899fdf0e8dc13d7fdc
PalagiriNagayogeswari <yogi34343@gmail.com>
2026-06-22 11:09:35 +0530

    [Nagayogeswari]:Implemented UC1 of diplaying OOPS to the console

---
+++ OOPSBannerApp.java
@@ -0,0 +1,13 @@
+/** OOPSBannerApp UC1 -OOPS Banner Display Application
+ * this class demonstrates a simple java application that displays the object oriented programming System
+ * OOPS acronym to the console.
+ *
+ */
+
+
+
+public class OOPSBannerApp {
+    public static void main(String[] args){
+        System.out.println("OOPS");
+    }
+}
